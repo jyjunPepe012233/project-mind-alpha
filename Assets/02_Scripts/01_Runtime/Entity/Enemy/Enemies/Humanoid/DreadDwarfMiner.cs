0768930fee262cb10ca7000b67871122cde3a0a5
jyjun_080808 <jinyj0217@gmail.com>
2025-04-17 23:35:02 +0900

    feat :: Add DreadDwarfMiner enemy

--- Assets/02_Scripts/01_Runtime/Entity/Enemy/Enemies/Humanoid/DreadDwarfMiner.cs
+++ Assets/02_Scripts/01_Runtime/Entity/Enemy/Enemies/Humanoid/DreadDwarfMiner.cs
@@ -1,10 +1,23 @@
-using MinD.SO.StatusFX.Effects;
-using NotImplementedException = System.NotImplementedException;
+using UnityEngine;
 
 namespace MinD.Runtime.Entity.Enemies {
 
-public class DreadDwarfMiner : HumanoidEnemy {
-	
-}
+	public class DreadDwarfMiner : HumanoidEnemy
+	{
+		[Header("Dread Dwarf Miner")]
+		[SerializeField] private ParticleSystem _quakeSelfVfx;
+		
+		public void SummonQuake()
+		{
+			_quakeSelfVfx.Play();
+			if (currentTarget != null)
+			{
+				GameObject quake = utility.InstantiatePrefab("DreadDwarfMiner_Quake");
+				quake.transform.position = currentTarget.transform.position;
+				quake.transform.forward = currentTarget.transform.position - transform.position;
+				quake.GetComponent<DreadDwarfMinerQuake>().SetOwner(this);
+			}
+		}
+	}
 
 }
