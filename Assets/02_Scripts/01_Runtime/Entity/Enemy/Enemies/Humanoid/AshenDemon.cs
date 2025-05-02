b9921cf1b876cd35bf7339be041cb7d32b7c7d25
jyjun_080808 <jinyj0217@gmail.com>
2025-05-03 00:08:47 +0900

    fix :: Fix fsm attributes of Ashen Demon

--- Assets/02_Scripts/01_Runtime/Entity/Enemy/Enemies/Humanoid/AshenDemon.cs
+++ Assets/02_Scripts/01_Runtime/Entity/Enemy/Enemies/Humanoid/AshenDemon.cs
@@ -1,10 +1,33 @@
+using MinD.Runtime.Object;
 using UnityEngine;
 
 namespace MinD.Runtime.Entity.Enemies {
 
 	public class AshenDemon : HumanoidEnemy
 	{
+		[Header("Ashen Demon")]
+		[SerializeField] private ParticleSystem swordTrail;
+		[SerializeField] private GameObject swordTrailActiveReference;
 		
-	}
+		private void CallOmen()
+		{
+			AshenDemonOmen[] omens = utility.InstantiatePrefabOnSelf("AshenDemon_Omens").GetComponentsInChildren<AshenDemonOmen>();
+			
+			foreach (var o in omens)
+			{
+				o.Setup(this, currentTarget);
+			}
+		}
 
+		private void Update()
+		{
+			if (swordTrail.isPlaying && !swordTrailActiveReference.activeInHierarchy)
+			{
+				swordTrail.Stop();
+			} else if (!swordTrail.isPlaying && swordTrailActiveReference.activeInHierarchy)
+			{
+				swordTrail.Play();
+			}
+		}
+	}
 }
