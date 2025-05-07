a5acbd651eb3ed2ee247002afc7d18aaafdcc79d
leewoojin1234 <geonui542@gmail.com>
2025-05-07 13:31:55 +0900

    Revert "feat :: FogColorChanger, Controller"
    This reverts commit 86437ba2f5d055a61d981b8b9514c2ad0869b608.

--- Assets/03_Prefabs/Objects/FogColorChanger.cs
+++ Assets/03_Prefabs/Objects/FogColorChanger.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class FogColorChanger : MonoBehaviour
 {
     public Color targetFogColor = Color.red;
-    public float duration = 2f;
-
+    
     private void OnTriggerEnter(Collider other)
     {
-        FogColorController.ChangeFogColor(this, targetFogColor, duration);
+        ChangeFogColor();
+    }
+    
+    private void ChangeFogColor()
+    {
+        RenderSettings.fogColor = targetFogColor;
     }
 }
