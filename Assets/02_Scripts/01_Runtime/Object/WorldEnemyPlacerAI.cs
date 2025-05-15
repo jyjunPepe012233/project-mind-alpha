cf26f308c8c84284ee6b094624a5cafb7ac4f0c9
jyjun_080808 <jinyj0217@gmail.com>
2025-05-15 15:28:14 +0900

    hotfix :: complement lastest commit

--- Assets/02_Scripts/01_Runtime/Object/WorldEnemyPlacerAI.cs
+++ Assets/02_Scripts/01_Runtime/Object/WorldEnemyPlacerAI.cs
@@ -74,6 +74,7 @@ public class WorldEnemyPlacerAI : MonoBehaviour
     }
     
     
+    #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
         void ShowUserData(Vector3 position, UserInformationSo data, GUIStyle style)
@@ -115,39 +116,6 @@ public class WorldEnemyPlacerAI : MonoBehaviour
             ShowUserData(reference.targetPoint.position + HandleUtility.GetHandleSize(reference.targetPoint.position) * Vector3.up * 0.5f, reference.userInformationSo, style);
 
         }
-        
-//        if (referenceUserInfo != null && referenceUserInfo.Length > 0)
-//        {
-//            Vector3 predictedPosition = Vector3.zero;
-//
-//            // 입력 벡터와 유사도를 기반으로 최종 위치 도출
-//            if (example != null)
-//            {
-//                UserInformationSo userInfo = example; // 임시로 첫 번째 사용자 정보 사용
-//                predictedPosition = CalculatePosition(userInfo);
-//            }
-//
-//            // predictedPosition을 그리기 전에, 각 Reference와 그려지는 선의 색상을 계산
-//            foreach (var reference in referenceUserInfo)
-//            {
-//                // Reference와 예측된 위치 간에 선을 그리기 전에, 가중치와 색상 계산
-//                Gizmos.color = reference.color; // 각 reference의 색상
-//                Gizmos.DrawWireSphere(reference.targetPoint.position, 0.5f); // targetPoint의 위치에 원 그리기
-//
-//                // 예측된 위치와 연결된 선 그리기 (색상 보간)
-//                Color lineColor = Color.Lerp(reference.color, Color.green, reference.weight); // 가중치에 따른 색상 보간
-//                Gizmos.color = lineColor;
-//                Gizmos.DrawLine(reference.targetPoint.position, predictedPosition); // 예측된 위치와 연결되는 선 그리기
-//
-//                // 참조 위치 근처에 텍스트로 색상/가중치/데미지 효율성 표시
-//                Handles.Label(reference.targetPoint.position + Vector3.up, $"{reference.userInformationSo.name}\n Weight: {reference.weight:F2}");
-//            }
-//
-//            // 예측된 위치 표시
-//            Gizmos.color = new Color(0, 1, 0, 0.3f); // 예측된 위치는 초록색
-//            Gizmos.DrawSphere(predictedPosition, HandleUtility.GetHandleSize(predictedPosition) * 0.3f); // 예측된 위치에 구 그리기
-//
-//            Handles.Label(predictedPosition + Vector3.up * HandleUtility.GetHandleSize(predictedPosition) * 1f, $"Predicted Position"); // 예측된 위치 레이블
-//        }
     }
+    #endif
 }
